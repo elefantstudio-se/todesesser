06010ff7e67eed6bb7429c0a9ea46ddec2f95e78
Gardiner91 <Gardiner91@d55067f0-5d91-e618-5bbf-3e60e31af2c1>
2010-06-04 01:51:50 +0000

    - Rotation kind of done. Works in Bottom Left and Top Right of screen.

---
+++ Todesesser/TodesesserGame/TodesesserGame/Core/GameFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Todesesser.Core
+{
+    public class GameFunctions
+    {
+        public static double GetAngle(Vector2 pos1, Vector2 pos2)
+        {
+            double feta = Math.Atan((pos2.Y - pos1.Y) / (pos2.X - pos1.X));
+            if (pos2.Y < pos1.Y)
+            {
+                feta += MathHelper.ToRadians(180);
+            }
+            return feta + MathHelper.ToRadians(90);
+        }
+    }
+}
