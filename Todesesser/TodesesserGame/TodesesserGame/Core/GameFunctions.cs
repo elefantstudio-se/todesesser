696919021d3de7f5ddeb6a56d27a2cb0a8559968
Gardiner91 <Gardiner91@d55067f0-5d91-e618-5bbf-3e60e31af2c1>
2010-06-11 01:52:06 +0000

    - Added Aim Line Debugging.

--- Todesesser/TodesesserGame/TodesesserGame/Core/GameFunctions.cs
+++ Todesesser/TodesesserGame/TodesesserGame/Core/GameFunctions.cs
@@ -60,5 +60,53 @@ namespace Todesesser.Core
 
             batch.Draw(spr, new Vector2(a.X - offset.X, a.Y - offset.Y), null, col, angle, Origin, Scale, SpriteEffects.None, 1.0f);
         }
+
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T c = a;
+            a = b;
+            b = c;
+        }
+
+        public static List<Point> BresenhamLine(int x0, int y0, int x1, int y1)
+        {
+            // Optimization: it would be preferable to calculate in 
+            // advance the size of "result" and to use a fixed-size array 
+            // instead of a list. 
+
+            List<Point> result = new List<Point>();
+
+            bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            if (steep)
+            {
+                Swap(ref x0, ref y0);
+                Swap(ref x1, ref y1);
+            }
+            if (x0 > x1)
+            {
+                Swap(ref x0, ref x1);
+                Swap(ref y0, ref y1);
+            }
+
+            int deltax = x1 - x0;
+            int deltay = Math.Abs(y1 - y0);
+            int error = 0;
+            int ystep;
+            int y = y0;
+            if (y0 < y1) ystep = 1; else ystep = -1;
+            for (int x = x0; x <= x1; x++)
+            {
+                if (steep) result.Add(new Point(y, x));
+                else result.Add(new Point(x, y));
+                error += deltay;
+                if (2 * error >= deltax)
+                {
+                    y += ystep;
+                    error -= deltax;
+                }
+            }
+
+            return result;
+        }
     }
 }
