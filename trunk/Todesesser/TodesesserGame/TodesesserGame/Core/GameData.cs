23fb80f874ccd9cb871f8876d42638bd0025ad48
Gardiner91 <Gardiner91@d55067f0-5d91-e618-5bbf-3e60e31af2c1>
2010-06-16 07:41:56 +0000

    - Added GameStats Class. - Added ScreenSize to GameData.

--- trunk/Todesesser/TodesesserGame/TodesesserGame/Core/GameData.cs
+++ trunk/Todesesser/TodesesserGame/TodesesserGame/Core/GameData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Todesesser.Core
 {
@@ -10,8 +11,10 @@ namespace Todesesser.Core
     {
         public enum GameStates { Invalid, Menu, Playing, Paused, Exiting, Splash };
         private static GameStates gameState = GameStates.Invalid;
-
         private static Rectangle mouseRect = new Rectangle();
+        private static ConPlug.ConPlug settings;
+        private static ConPlug.ConPlug profile;
+        private static Viewport screenSize;
 
         public static GameStates GameState
         {
@@ -23,6 +26,24 @@ namespace Todesesser.Core
         {
             get { return mouseRect; }
             set { mouseRect = value; }
+        }
+
+        public static ConPlug.ConPlug Settings
+        {
+            get { return settings; }
+            set { settings = value; }
+        }
+
+        public static ConPlug.ConPlug Profile
+        {
+            get { return profile; }
+            set { profile = value; }
+        }
+
+        public static Viewport ScreenSize
+        {
+            get { return screenSize; }
+            set { screenSize = value; }
         }
     }
 }
