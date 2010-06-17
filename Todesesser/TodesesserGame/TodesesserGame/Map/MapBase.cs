ce72c6e8646ccc432ecd441b872daabbd83a52f8
Gardiner91 <Gardiner91@d55067f0-5d91-e618-5bbf-3e60e31af2c1>
2010-06-17 08:19:08 +0000

    - Infinite Ground Tileing Done! Finally! - TODO: Edit Ground Tileing to support bigger resolutions.

--- Todesesser/TodesesserGame/TodesesserGame/Map/MapBase.cs
+++ Todesesser/TodesesserGame/TodesesserGame/Map/MapBase.cs
@@ -40,16 +40,81 @@ namespace Todesesser.Map
             Content.AddTexture2D("Enviroment\\Ground", groundContentKey);
             //Create Enviroment Ground Objects to cover the screen
             //TODO: Finish Ground Updating!
-            //groundObjects.Add("eg-0", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-0", groundContentKey));
-            //((ObjectGround)groundObjects["eg-0"]).Position = new Vector2(0, -512);
-            //groundObjects.Add("eg-1", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-1", groundContentKey));
-            //((ObjectGround)groundObjects["eg-1"]).Position = new Vector2(0, -256);
-            //groundObjects.Add("eg-2", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-2", groundContentKey));
-            //((ObjectGround)groundObjects["eg-2"]).Position = new Vector2(0, 0);
-            //groundObjects.Add("eg-3", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-3", groundContentKey));
-            //((ObjectGround)groundObjects["eg-3"]).Position = new Vector2(0, 256);
-            //groundObjects.Add("eg-4", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-4", groundContentKey));
-            //((ObjectGround)groundObjects["eg-4"]).Position = new Vector2(0, 512);
+            //==============================
+            //Y = -256
+            //==============================
+            groundObjects.Add("eg-2", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-2", groundContentKey));
+            ((ObjectGround)groundObjects["eg-2"]).Position = new Vector2(-256, -256);
+            groundObjects.Add("eg-3", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-3", groundContentKey));
+            ((ObjectGround)groundObjects["eg-3"]).Position = new Vector2(0, -256);
+            groundObjects.Add("eg-4", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-4", groundContentKey));
+            ((ObjectGround)groundObjects["eg-4"]).Position = new Vector2(256, -256);
+            groundObjects.Add("eg-5", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-5", groundContentKey));
+            ((ObjectGround)groundObjects["eg-5"]).Position = new Vector2(512, -256);
+            groundObjects.Add("eg-6", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-6", groundContentKey));
+            ((ObjectGround)groundObjects["eg-6"]).Position = new Vector2(768, -256);
+            groundObjects.Add("eg-7", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-7", groundContentKey));
+            ((ObjectGround)groundObjects["eg-7"]).Position = new Vector2(1024, -256);
+            //==============================
+            //Y = 0
+            //==============================
+            groundObjects.Add("eg-8", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-8", groundContentKey));
+            ((ObjectGround)groundObjects["eg-8"]).Position = new Vector2(-256, 0);
+            groundObjects.Add("eg-9", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-9", groundContentKey));
+            ((ObjectGround)groundObjects["eg-9"]).Position = new Vector2(0, 0);
+            groundObjects.Add("eg-10", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-10", groundContentKey));
+            ((ObjectGround)groundObjects["eg-10"]).Position = new Vector2(256, 0);
+            groundObjects.Add("eg-11", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-11", groundContentKey));
+            ((ObjectGround)groundObjects["eg-11"]).Position = new Vector2(512, 0);
+            groundObjects.Add("eg-12", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-12", groundContentKey));
+            ((ObjectGround)groundObjects["eg-12"]).Position = new Vector2(768, 0);
+            groundObjects.Add("eg-13", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-13", groundContentKey));
+            ((ObjectGround)groundObjects["eg-13"]).Position = new Vector2(1024, 0);
+            //==============================
+            //Y = 256
+            //==============================
+            groundObjects.Add("eg-14", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-14", groundContentKey));
+            ((ObjectGround)groundObjects["eg-14"]).Position = new Vector2(-256, 256);
+            groundObjects.Add("eg-15", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-15", groundContentKey));
+            ((ObjectGround)groundObjects["eg-15"]).Position = new Vector2(0, 256);
+            groundObjects.Add("eg-16", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-16", groundContentKey));
+            ((ObjectGround)groundObjects["eg-16"]).Position = new Vector2(256, 256);
+            groundObjects.Add("eg-17", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-17", groundContentKey));
+            ((ObjectGround)groundObjects["eg-17"]).Position = new Vector2(512, 256);
+            groundObjects.Add("eg-18", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-18", groundContentKey));
+            ((ObjectGround)groundObjects["eg-18"]).Position = new Vector2(768, 256);
+            groundObjects.Add("eg-19", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-19", groundContentKey));
+            ((ObjectGround)groundObjects["eg-19"]).Position = new Vector2(1024, 256);
+            //==============================
+            //Y = 512
+            //==============================
+            groundObjects.Add("eg-20", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-20", groundContentKey));
+            ((ObjectGround)groundObjects["eg-20"]).Position = new Vector2(-256, 512);
+            groundObjects.Add("eg-21", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-21", groundContentKey));
+            ((ObjectGround)groundObjects["eg-21"]).Position = new Vector2(0, 512);
+            groundObjects.Add("eg-22", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-22", groundContentKey));
+            ((ObjectGround)groundObjects["eg-22"]).Position = new Vector2(256, 512);
+            groundObjects.Add("eg-23", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-23", groundContentKey));
+            ((ObjectGround)groundObjects["eg-23"]).Position = new Vector2(512, 512);
+            groundObjects.Add("eg-24", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-24", groundContentKey));
+            ((ObjectGround)groundObjects["eg-24"]).Position = new Vector2(768, 512);
+            groundObjects.Add("eg-25", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-25", groundContentKey));
+            ((ObjectGround)groundObjects["eg-25"]).Position = new Vector2(1024, 512);
+            //==============================
+            //Y = 768
+            //==============================
+            groundObjects.Add("eg-26", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-26", groundContentKey));
+            ((ObjectGround)groundObjects["eg-26"]).Position = new Vector2(-256, 768);
+            groundObjects.Add("eg-27", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-27", groundContentKey));
+            ((ObjectGround)groundObjects["eg-27"]).Position = new Vector2(0, 768);
+            groundObjects.Add("eg-28", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-28", groundContentKey));
+            ((ObjectGround)groundObjects["eg-28"]).Position = new Vector2(256, 768);
+            groundObjects.Add("eg-29", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-29", groundContentKey));
+            ((ObjectGround)groundObjects["eg-29"]).Position = new Vector2(512, 768);
+            groundObjects.Add("eg-30", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-30", groundContentKey));
+            ((ObjectGround)groundObjects["eg-30"]).Position = new Vector2(768, 768);
+            groundObjects.Add("eg-31", ObjectPool.AddObject(ObjectPooling.ObjectPool.ObjectTypes.Ground, "eg-31", groundContentKey));
+            ((ObjectGround)groundObjects["eg-31"]).Position = new Vector2(1024, 768);
         }
 
         public virtual void LoadContent()
