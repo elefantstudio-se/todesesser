c6e014ddef90f01a2d40b0e74212cefda5520d47
Gardiner91 <Gardiner91@d55067f0-5d91-e618-5bbf-3e60e31af2c1>
2010-06-23 19:30:25 +0000

    TODLIGHTING:  - Added Base Code.

---
+++ branches/TodLighting/Lighting/GameFunctions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TodLighting
+{
+    public class GameFunctions
+    {
+        public static double GetAngle(Vector2 pos1, Vector2 pos2)
+        {
+            return (float)Math.Atan2((pos2.Y - pos1.Y), (pos2.X - pos1.X)) - MathHelper.PiOver2;
+        }
+
+        public static void DrawLine(SpriteBatch batch, Texture2D spr, Vector2 a, Vector2 b, Color col)
+        {
+            Vector2 Origin = new Vector2(0.5f, 0.0f);
+            Vector2 diff = b - a;
+            float angle;
+            Vector2 Scale = new Vector2(1.0f, diff.Length() / spr.Height);
+
+            angle = (float)(Math.Atan2(diff.Y, diff.X)) - MathHelper.PiOver2;
+
+            batch.Draw(spr, a, null, col, angle, Origin, Scale, SpriteEffects.None, 1.0f);
+        }
+
+        public static Vector2 GetVectorProjection(Vector2 start, int rotation, int distance)
+        {
+            double x = Math.Sin(MathHelper.ToRadians(rotation));
+            double y = Math.Cos(MathHelper.ToRadians(rotation));
+            x = x * distance;
+            y = y * distance;
+            x = start.X + x;
+            y = start.Y + y;
+
+            return new Vector2(float.Parse(x.ToString()), float.Parse(y.ToString()));
+        }
+    }
+}
---
+++ branches/TodLighting/Lighting/LightingTypes/AmbientLight.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TodLighting;
+using System.Diagnostics;
+using System.Collections;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lighting.LightingTypes
+{
+    public class AmbientLight
+    {
+        //Variables
+        private Vector2 position;
+        private float range;
+        private Color colour;
+        private Texture2D lightTexture;
+
+        private Vector2 hullAngles;
+        private Texture2D lineTexture;
+
+        private double rotation = 0;
+
+        private VertexPositionColor[] vertices;
+        private Int16[] indices;
+
+        private IndexBuffer iBuffer;
+        private VertexBuffer vBuffer;
+
+        private Effect basicEffect;
+
+        //Methods
+        public AmbientLight(GraphicsDevice device, Texture2D texture, Effect basicEffect, Color colour, float range, Vector2 position, Texture2D lineTexture)
+        {
+            this.basicEffect = basicEffect;
+            this.position = position;
+            this.range = range;
+            this.colour = colour;
+            this.lightTexture = texture;
+            this.lineTexture = lineTexture;
+
+            vertices = new VertexPositionColor[5];
+            vertices[0].Position = new Vector3(0f, 0f, 0f);
+            vertices[0].Color = Color.White;
+            vertices[1].Position = new Vector3(5f, 0f, 0f);
+            vertices[1].Color = Color.White;
+            vertices[2].Position = new Vector3(10f, 0f, 0f);
+            vertices[2].Color = Color.White;
+            vertices[3].Position = new Vector3(5f, 0f, -5f);
+            vertices[3].Color = Color.White;
+            vertices[4].Position = new Vector3(10f, 0f, -5f);
+            vertices[4].Color = Color.White;
+            vBuffer = new VertexBuffer(device, typeof(VertexPositionColor), vertices.Length, BufferUsage.WriteOnly);
+            vBuffer.SetData<VertexPositionColor>(vertices);
+
+            indices = new Int16[6];
+            indices[0] = 3;
+            indices[1] = 1;
+            indices[2] = 0;
+            indices[3] = 4;
+            indices[4] = 2;
+            indices[5] = 1;
+            iBuffer = new IndexBuffer(device,typeof(Int16), indices.Length, BufferUsage.WriteOnly);
+            iBuffer.SetData<Int16>(indices, 0, indices.Length);
+        }
+
+        public void Update(GameTime gameTime, BaseHull[] hulls)
+        {
+            if (rotation < 360)
+            {
+                rotation += MathHelper.ToRadians(30);
+            }
+            else
+            {
+                rotation = MathHelper.ToRadians(0);
+            }
+
+            foreach (BaseHull hull in hulls)
+            {
+                //Get Angle from light to hull.
+                double angle = GameFunctions.GetAngle(Position, hull.Position);
+                hullAngles = hull.Position;
+            }
+        }
+
+        public void Draw(GraphicsDevice device, SpriteBatch spriteBatch, BaseHull[] hulls)
+        {
+            Vector2 center = new Vector2(lightTexture.Width / 2, lightTexture.Height / 2);
+            float scale = range / ((float)lightTexture.Width / 2.0f);
+            spriteBatch.Draw(lightTexture, position, null, colour, 0, center, scale, SpriteEffects.None, 1);
+
+            if (hulls.Length >= 1)
+            {
+                foreach (BaseHull hull in hulls)
+                {
+                    GameFunctions.DrawLine(spriteBatch, lineTexture, Position, new Vector2(hull.Position.X, hull.Position.Y + hull.Size.Height), Color.Red);
+                    GameFunctions.DrawLine(spriteBatch, lineTexture, Position, new Vector2(hull.Position.X + hull.Size.Width, hull.Position.Y), Color.Green);
+
+                    double angle1 = GameFunctions.GetAngle(Position, new Vector2(hull.Position.X, hull.Position.Y + hull.Size.Height));
+                    double angle2 = GameFunctions.GetAngle(Position, new Vector2(hull.Position.X + hull.Size.Width, hull.Position.Y));
+
+                    int rot1 = Convert.ToInt32(MathHelper.ToDegrees(float.Parse(angle1.ToString())));
+                    rot1 = 0 - rot1;
+                    int rot2 = Convert.ToInt32(MathHelper.ToDegrees(float.Parse(angle2.ToString())));
+                    rot2 = 0 - rot2;
+
+                    Vector2 projection1 = GameFunctions.GetVectorProjection(new Vector2(hull.Position.X, hull.Position.Y + hull.Size.Height), rot1, 100);
+                    Vector2 projection2 = GameFunctions.GetVectorProjection(new Vector2(hull.Position.X + hull.Size.Width, hull.Position.Y), rot2, 100);
+
+                    GameFunctions.DrawLine(spriteBatch, lineTexture, new Vector2(hull.Position.X, hull.Position.Y + hull.Size.Height), projection1, Color.Red);
+                    GameFunctions.DrawLine(spriteBatch, lineTexture, new Vector2(hull.Position.X + hull.Size.Width, hull.Position.Y), projection2, Color.Green); 
+                }
+            }
+
+            foreach (EffectTechnique technique in basicEffect.Techniques)
+            {
+                foreach (EffectPass pass in technique.Passes)
+                {
+                    pass.Apply();
+                    device.DrawUserIndexedPrimitives<VertexPositionColor>(PrimitiveType.TriangleStrip, vertices, 0, vertices.Length, indices, 0, indices.Length / 3);
+                    Debug.WriteLine("Draw Primitives:");
+                }
+            }
+        }
+
+        //Properties
+        public Vector2 Position
+        {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+        public float Range
+        {
+            get { return this.range; }
+            set { this.range = value; }
+        }
+        public Color Colour
+        {
+            get { return this.colour; }
+            set { this.colour = value; }
+        }
+        public Texture2D LightTexture
+        {
+            get { return this.lightTexture; }
+            set { this.lightTexture = value; }
+        }
+    }
+}
