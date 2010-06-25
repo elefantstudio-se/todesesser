ef62b0f5436859888fe192880d509b31c3cada66
Gardiner91 <Gardiner91@d55067f0-5d91-e618-5bbf-3e60e31af2c1>
2010-06-25 10:41:54 +0000

    - GPU2D Engine Added.

--- TodLighting/TodLighting/Game1.cs
+++ TodLighting/TodLighting/Game1.cs
@@ -1,3 +1,23 @@
+//Todesesser, XNA 4.0 C# Game.
+//Copyright (C) 2010  Dean Gardiner and Taylor Lodge.
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//Email: gardiner91@gmail.com.
+//
+//Full Licence can be found at http://www.gnu.org/licenses/gpl-3.0.txt.
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -74,10 +94,6 @@ namespace TodLighting
                     int y = 300 + Convert.ToInt32(15 * Math.Cos(t / 5));
                     objectHulls[0].Position = new Vector2(x, y);
                     t += 1;
-                    if (objectHulls[0].Position.X > 800)
-                    {
-                        int j = 0;
-                    }
                 }
                 else
                 {
