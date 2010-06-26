c2ddb4ad110690e553547291596b988438d11dc0
Gardiner91 <Gardiner91@d55067f0-5d91-e618-5bbf-3e60e31af2c1>
2010-06-26 00:50:37 +0000

    - Added menu Background image switching.

--- trunk/Todesesser/TodesesserGame/TodesesserGame/Screens/MenuScreen.cs
+++ trunk/Todesesser/TodesesserGame/TodesesserGame/Screens/MenuScreen.cs
@@ -36,23 +36,30 @@ namespace Todesesser.Screens
 
         public override void LoadContent()
         {
-            //Load Buttons
+            #region Load Buttons
             Content.AddTexture2D("Buttons\\Menu\\NewGame", "Buttons-Menu-NewGame");
             Content.AddTexture2D("Buttons\\Menu\\NewGameSelected", "Buttons-Menu-NewGame_Selected");
             buttonNewGame = (ObjectButton)Objects.AddObject(ObjectPool.ObjectTypes.Button, "Buttons-Menu-NewGame", "Buttons-Menu-NewGame");
             buttonNewGame.X = 50;
-            buttonNewGame.Y = 20;
+            buttonNewGame.Y = 220;
             buttonNewGame.OnClick += new ObjectButton.OnClickDelegate(button_OnClick);
 
             Content.AddTexture2D("Buttons\\Menu\\Exit", "Buttons-Menu-Exit");
             Content.AddTexture2D("Buttons\\Menu\\ExitSelected", "Buttons-Menu-Exit_Selected");
             buttonExit = (ObjectButton)Objects.AddObject(ObjectPool.ObjectTypes.Button, "Buttons-Menu-Exit", "Buttons-Menu-Exit");
             buttonExit.X = 50;
-            buttonExit.Y = 90;
+            buttonExit.Y = 290;
             buttonExit.OnClick += new ObjectButton.OnClickDelegate(button_OnClick);
+            #endregion
             //Load Cursor
             Content.AddTexture2D("Cursors\\Basic", "MenuCursor");
             cursor = (ObjectCursor)Objects.AddObject(ObjectPool.ObjectTypes.Cursor, "MenuCursor", "MenuCursor");
+            #region Load Menu Backgrounds
+            Content.AddTexture2D("Menu\\todesesser_menu_1080p", "menu1080p");
+            Content.AddTexture2D("Menu\\todesesser_menu_900p", "menu900p");
+            Content.AddTexture2D("Menu\\todesesser_menu_720p", "menu720p");
+            Content.AddTexture2D("Menu\\todesesser_menu_800x600", "menu800x600");
+            #endregion
 
             base.LoadContent();
         }
@@ -84,6 +91,34 @@ namespace Todesesser.Screens
             GraphicsDevice.Clear(Color.White);
 
             Batch.Begin();
+
+            switch (Batch.GraphicsDevice.Viewport.Height)
+            {
+                case 1080:
+                    if (Batch.GraphicsDevice.Viewport.Width == 1920)
+                    {
+                        Batch.Draw(Content.GetTexture2D("menu1080p").Texture, new Rectangle(0, 0, 1920, 1080), Color.White);
+                    }
+                    break;
+                case 900:
+                    if (Batch.GraphicsDevice.Viewport.Width == 1440)
+                    {
+                        Batch.Draw(Content.GetTexture2D("menu900p").Texture, new Rectangle(0, 0, 1440, 900), Color.White);
+                    }
+                    break;
+                case 720:
+                    if (Batch.GraphicsDevice.Viewport.Width == 1280)
+                    {
+                        Batch.Draw(Content.GetTexture2D("menu720p").Texture, new Rectangle(0, 0, 1280, 720), Color.White);
+                    }
+                    break;
+                case 600:
+                    if (Batch.GraphicsDevice.Viewport.Width == 800)
+                    {
+                        Batch.Draw(Content.GetTexture2D("menu800x600").Texture, new Rectangle(0, 0, 800, 600), Color.White);
+                    }
+                    break;
+            }
 
             buttonNewGame.Draw(gameTime, Batch);
             buttonExit.Draw(gameTime, Batch);
