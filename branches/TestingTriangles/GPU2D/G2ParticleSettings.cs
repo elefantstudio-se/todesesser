3d37656967f9c7fd5b635ef668489926597b172c
Gardiner91 <Gardiner91@d55067f0-5d91-e618-5bbf-3e60e31af2c1>
2010-06-30 00:07:07 +0000

    Started work on Particles

---
+++ branches/TestingTriangles/GPU2D/G2ParticleSettings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GPU2D
+{
+    public class G2ParticleSettings
+    {
+        public string textureName = null;
+        
+        public int maxParticles = 100;
+
+        public TimeSpan duration = TimeSpan.FromSeconds(1);
+
+        public float durationRandomness = 0;
+
+        public float emitterVelocitySensitivity = 1;
+
+        public float minHorizontalVelocity = 0;
+        public float maxHorizontalVelocity = 0;
+
+        public float minVerticalVelocity = 0;
+        public float maxVerticalVelocity = 0;
+
+        public Vector3 gravity = Vector3.Zero;
+
+        public float endVelocity = 1;
+
+        public Color minColour = Color.White;
+        public Color maxColour = Color.White;
+
+        public float minRotateSpeed = 0;
+        public float maxRotateSpeed = 0;
+
+        public float minStartSize = 100;
+        public float maxStartSize = 100;
+
+        public float minEndSize = 100;
+        public float maxEndSize = 100;
+
+        public BlendState blend = BlendState.AlphaBlend;
+    }
+}
---
+++ branches/TestingTriangles/GPU2D/ParticleVertex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GPU2D
+{
+    struct ParticleVertex
+    {
+        public Vector3 Position;
+
+        public Vector3 Velocity;
+
+        public Vector2 Offset;
+
+        public Color Random;
+
+        public float Time;
+
+        public static readonly VertexElement[] VertexElements = {
+                                                                    new VertexElement(0, VertexElementFormat.Vector3,
+                                                                        VertexElementUsage.Position, 0),
+
+                                                                    new VertexElement(12, VertexElementFormat.Vector3,
+                                                                        VertexElementUsage.Normal, 0),
+
+                                                                    new VertexElement(24, VertexElementFormat.Vector2,
+                                                                        VertexElementUsage.PointSize, 0),
+
+                                                                    new VertexElement(32, VertexElementFormat.Color,
+                                                                        VertexElementUsage.Color, 0),
+                                                                    new VertexElement(36, VertexElementFormat.Single,
+                                                                        VertexElementUsage.TextureCoordinate, 0),
+                                                                };
+
+        public const int SizeInBytes = 40;
+    }
+}
