b334edd6c484a06c8a550b8873b2811eacd2e83c
Gardiner91 <Gardiner91@d55067f0-5d91-e618-5bbf-3e60e31af2c1>
2010-07-01 10:35:50 +0000

    - Working Paticle Engine! Yay! (Still needs implementing).

--- TodParticles/TodParticles/Game1.cs
+++ TodParticles/TodParticles/Game1.cs
@@ -11,6 +11,7 @@ using Microsoft.Xna.Framework.Input.Touch;
 using Microsoft.Xna.Framework.Media;
 using GPU2D;
 using GPU2D.Particles;
+using GPU2D.VParticle;
 
 namespace TodParticles
 {
@@ -18,52 +19,12 @@ namespace TodParticles
     {
         #region Fields
 
-
         GraphicsDeviceManager graphics;
 
         SpriteBatch spriteBatch;
-        SpriteFont font;
-        Model grid;
 
-
-        // This sample uses five different particle systems.
-        ParticleSystem explosionParticles;
-        ParticleSystem explosionSmokeParticles;
-        ParticleSystem projectileTrailParticles;
-        ParticleSystem smokePlumeParticles;
-        ParticleSystem fireParticles;
-
-
-        // The sample can switch between three different visual effects.
-        enum ParticleState
-        {
-            Explosions,
-            SmokePlume,
-            RingOfFire,
-        };
-        
-        ParticleState currentState = ParticleState.Explosions;
-
-
-        // The explosions effect works by firing projectiles up into the
-        // air, so we need to keep track of all the active projectiles.
-        List<Projectile> projectiles = new List<Projectile>();
-
-        TimeSpan timeToNextProjectile = TimeSpan.Zero;
-
-
-        // Random number generator for the fire effect.
-        Random random = new Random();
-
-        float cameraHeight = 5f;
-
-
-        // Input state.
-        KeyboardState currentKeyboardState;
-        GamePadState currentGamePadState;
-
-        KeyboardState lastKeyboardState;
-        GamePadState lastGamePadState;
+        //GPU2D:
+        GPU2DEngine gpu2d;
 
         #endregion
 
@@ -79,28 +40,6 @@ namespace TodParticles
 
             graphics.PreferredBackBufferWidth = 853;
             graphics.PreferredBackBufferHeight = 480;
-
-            // Construct our particle system components.
-            explosionParticles = new G2ParticleExplosion(this, Content);
-            explosionSmokeParticles = new G2ParticleExplosionSmoke(this, Content);
-            projectileTrailParticles = new G2ProjectileTrail(this, Content);
-            smokePlumeParticles = new G2SmokePlume(this, Content);
-            fireParticles = new G2ParticleFire(this, Content);
-
-            // Set the draw order so the explosions and fire
-            // will appear over the top of the smoke.
-            smokePlumeParticles.DrawOrder = 100;
-            explosionSmokeParticles.DrawOrder = 200;
-            projectileTrailParticles.DrawOrder = 300;
-            explosionParticles.DrawOrder = 400;
-            fireParticles.DrawOrder = 500;
-
-            // Register the particle system components.
-            Components.Add(explosionParticles);
-            Components.Add(explosionSmokeParticles);
-            Components.Add(projectileTrailParticles);
-            Components.Add(smokePlumeParticles);
-            Components.Add(fireParticles);
         }
 
 
@@ -110,9 +49,11 @@ namespace TodParticles
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(graphics.GraphicsDevice);
-
-            font = Content.Load<SpriteFont>("font");
-            grid = Content.Load<Model>("grid");
+            gpu2d = new GPU2DEngine(Content.Load<Effect>("Basic"), graphics);
+            gpu2d.AddParticle(new G2VSmokePlume(this, Content, new Vector2(200, 200)));
+            gpu2d.AddParticle(new G2VFire(this, Content, new Vector2(100,100)));
+            gpu2d.AddParticle(new G2VExplosion(this, Content, new Vector2(300, 300)));
+            gpu2d.AddParticle(new G2VFireRing(this, Content, new Vector2(400, 400), 40, 30));
         }
 
         #endregion
@@ -125,117 +66,10 @@ namespace TodParticles
         /// </summary>
         protected override void Update(GameTime gameTime)
         {
-            HandleInput();
-
-            switch (currentState)
-            {
-                case ParticleState.Explosions:
-                    UpdateExplosions(gameTime);
-                    break;
-
-                case ParticleState.SmokePlume:
-                    UpdateSmokePlume();
-                    break;
-
-                case ParticleState.RingOfFire:
-                    UpdateFire();
-                    break;
-            }
-
-            UpdateProjectiles(gameTime);
+            gpu2d.Update(gameTime);
 
             base.Update(gameTime);
         }
-
-
-        /// <summary>
-        /// Helper for updating the explosions effect.
-        /// </summary>
-        void UpdateExplosions(GameTime gameTime)
-        {
-            timeToNextProjectile -= gameTime.ElapsedGameTime;
-
-            if (timeToNextProjectile <= TimeSpan.Zero)
-            {
-                // Create a new projectile once per second. The real work of moving
-                // and creating particles is handled inside the Projectile class.
-                projectiles.Add(new Projectile(explosionParticles,
-                                               explosionSmokeParticles,
-                                               projectileTrailParticles));
-
-                timeToNextProjectile += TimeSpan.FromSeconds(1);
-            }
-        }
-
-
-        /// <summary>
-        /// Helper for updating the list of active projectiles.
-        /// </summary>
-        void UpdateProjectiles(GameTime gameTime)
-        {
-            int i = 0;
-
-            while (i < projectiles.Count)
-            {
-                if (!projectiles[i].Update(gameTime))
-                {
-                    // Remove projectiles at the end of their life.
-                    projectiles.RemoveAt(i);
-                }
-                else
-                {
-                    // Advance to the next projectile.
-                    i++;
-                }
-            }
-        }
-
-
-        /// <summary>
-        /// Helper for updating the smoke plume effect.
-        /// </summary>
-        void UpdateSmokePlume()
-        {
-            // This is trivial: we just create one new smoke particle per frame.
-            smokePlumeParticles.AddParticle(Vector3.Zero, Vector3.Zero);
-        }
-
-
-        /// <summary>
-        /// Helper for updating the fire effect.
-        /// </summary>
-        void UpdateFire()
-        {
-            const int fireParticlesPerFrame = 20;
-
-            // Create a number of fire particles, randomly positioned around a circle.
-            for (int i = 0; i < fireParticlesPerFrame; i++)
-            {
-                fireParticles.AddParticle(RandomPointOnCircle(), Vector3.Zero);
-            }
-
-            // Create one smoke particle per frmae, too.
-            smokePlumeParticles.AddParticle(RandomPointOnCircle(), Vector3.Zero);
-        }
-
-
-        /// <summary>
-        /// Helper used by the UpdateFire method. Chooses a random location
-        /// around a circle, at which a fire particle will be created.
-        /// </summary>
-        Vector3 RandomPointOnCircle()
-        {
-            const float radius = 30;
-            const float height = 40;
-
-            double angle = random.NextDouble() * Math.PI * 2;
-
-            float x = (float)Math.Cos(angle);
-            float y = (float)Math.Sin(angle);
-
-            return new Vector3(x * radius, y * radius + height, 0);
-        }
-
 
         /// <summary>
         /// This is called when the game should draw itself.
@@ -246,54 +80,9 @@ namespace TodParticles
 
             device.Clear(Color.CornflowerBlue);
 
-            Matrix view = Matrix.CreateLookAt(new Vector3(0,
-                                                          -cameraHeight,
-                                                          0),
-                                              new Vector3(0,
-                                                          0,
-                                                          0),
-                                              Vector3.UnitZ);
-            Matrix projection = Matrix.CreateOrthographic(graphics.PreferredBackBufferWidth, 
-                                                          graphics.PreferredBackBufferHeight, 
-                                                          1, 
-                                                          10000) * 
-                                Matrix.CreateScale(1);
-
-            // Pass camera matrices through to the particle system components.
-            explosionParticles.SetCamera(view, projection);
-            explosionSmokeParticles.SetCamera(view, projection);
-            projectileTrailParticles.SetCamera(view, projection);
-            smokePlumeParticles.SetCamera(view, projection);
-            fireParticles.SetCamera(view, projection);
+            gpu2d.Draw(gameTime, graphics);
 
             base.Draw(gameTime);
-        }
-
-        #endregion
-
-        #region Handle Input
-
-
-        /// <summary>
-        /// Handles input for quitting the game and cycling
-        /// through the different particle effects.
-        /// </summary>
-        void HandleInput()
-        {
-            lastKeyboardState = currentKeyboardState;
-            lastGamePadState = currentGamePadState;
-
-            currentKeyboardState = Keyboard.GetState();
-            currentGamePadState = GamePad.GetState(PlayerIndex.One);
-
-            // Check for changing the active particle effect.
-            if (currentKeyboardState.IsKeyDown(Keys.Space) && lastKeyboardState.IsKeyUp(Keys.Space))
-            {
-                currentState++;
-
-                if (currentState > ParticleState.RingOfFire)
-                    currentState = 0;
-            }
         }
 
         #endregion
